7eefa74f831043940c1c09b057ccc555e8ef40da
whitehox <oketegah@gmail.com>
2020-09-03 14:41:36 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import eyeson from 'eyeson';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  React.useEffect(() => {
+    eyeson.onEvent((event) => {
+      if (event.type !== 'accept') {
+        return;
+      }
+      // Note: Some iOS devices might require video to have autoplay attribute set.
+      let video = document.querySelector('video');
+      video.srcObject = event.remoteStream;
+      video.play();
+    });
+    eyeson.start('rwdWOeNn7IUbr8VlnfS6YxGu');
+  });
+  return <video className="video"></video>;
 }
 
 export default App;
